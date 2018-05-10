acc4e9218bcefe8f7f665797809d0ecd59a73dbf
Tobias Johansson <TobiasJohansson911@gmail.com>
2018-05-10 13:06:19 +0200

    first layout with dummydata

---
+++ klocka/CustomTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CustomTableViewCell.swift
+//  klocka
+//
+//  Created by lösen är 0000 on 2018-05-10.
+//  Copyright © 2018 TobiasJohansson. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- klocka/ViewController.swift
+++ klocka/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var clockView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath) as! CustomTableViewCell
+        cell.cityNameLabel.text = "Falkenberg"
+        cell.dateLabel.text = "10/5"
+        cell.timeLabel.text = "12:50"
+        return cell
+    }
+    
+    
+    
 }
 
